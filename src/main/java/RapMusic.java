b25780e00693fc59ac792b33ae6151cbedb28775
Sergey Zozulya <zozulia.sergey2011@yandex.ru>
2021-01-13 17:08:09 +0300

    first commit

---
+++ src/main/java/RapMusic.java
@@ -0,0 +1,6 @@
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Тащемта, реп - это кал, например";
+    }
+}
---
+++ src/main/java/TestSpring.java
@@ -0,0 +1,19 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        //Music music = context.getBean("musicBean", Music.class);
+
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        
+        System.out.println(musicPlayer.getVolume());
+
+        context.close();
+    }
+}
